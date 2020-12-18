4b6f29a973910007e745c1a45ef82570bff42fc6
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-12-18 16:25:14 -0500

    Edits made to 1_fw.py, simplified, added fw export bids.

--- 1_fw.py
+++ 1_fw.py
@@ -4,36 +4,20 @@
 # Count number of subjects on sophon with movies
 # Current count is 174
 
-import subprocess as sp 
+import subprocess as sp
 import glob
+import tqdm
 from settings import *
 fwls = [path + 'fw', 'ls']
-
-subs = [s.split(path)[-1][4:] for s in glob.glob(path+'sub*')]
+fwget = [path+'fw','export','bids',datapath,'--project','PACCT_study_w2','--session']
 
 fwlist = sp.check_output(fwls+[fwpath]).splitlines()
 fwlist = [x.decode("utf-8") for x in fwlist]
-fwlist = [s[3:] for s in fwlist if 'PA' in s and len(s)==8]
+fwlist = [s[1:] for s in fwlist if 'PA' in s and len(s)==6]
 
 # If anything prints, new subject with functional run has been added
-for sub in list(set(fwlist).difference(subs)):
-	subpath = fwpath + '/' + sub
-	try:
-		for ses_tmp in sp.check_output(fwls + [subpath]).splitlines():
-			ses = ses_tmp.decode("utf-8")[-10:]
-			if len([i.decode("utf-8") for i in sp.check_output(fwls + [subpath + '/' + ses]).splitlines() if 'func' in i.decode("utf-8")])>0:
-				print(sub,ses)
-	except:
-		continue
-		
-movsubs = [sub for sub in subs if len(glob.glob(path+'sub-'+sub+'/ses*/func/*'))>0]
+for sub in tqdm.tqdm(fwlist):#list(set(fwlist).difference(subs)):
+	out = sp.run(fwget+[sub+'_V2W2'])
+	print(sub,out)
 
-
-
-# Notes if need to download more subjects in future
-'''
-sp.run(["aws","s3","cp","s3://fcp-indi/data/Archives/HBN/MRI/"+site+"/%s"%(sub),path,"--no-sign-request"])
-fwpath export bids [Project in Flywheel] [Destination folder] [optional flags]
-/data/PACCT/fw export bids --project PACCT_study_w2 .
-'''
 
