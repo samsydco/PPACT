bc139a3ebf75daf9ef72c540c4b4545243876c0b
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-12-18 16:26:02 -0500

    Edited settings - added more paths.

--- settings.py
+++ settings.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+fwpath = 'tottenham/PACCT_study_w2'
 
 path = '/data/PACCT/'
-fwpath = 'tottenham/PACCT_study_w2'
+datapath = path+'data_2/'
+T1path = path+'T1_rating/'
+fmripreppath = path+'fmriprep_output/'
+
+
+
 
 
 
