b6d03a48aaa43349451d0c6ff4ee01a747857c4d
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-04-15 15:30:55 -0400

    modified settings.

--- settings.py
+++ settings.py
@@ -3,12 +3,14 @@
 fwpath = 'tottenham/PACCT_study_w2'
 
 path = '/data/PACCT/'
-datapath = path+'data_2/'
+datapath = path+'data/'
 T1path = path+'T1_rating/'
 outputdr = path+'fmriprep_output/'
 fmripreppath = outputdr+'fmriprep/'
 prepath = path+'Preprocessed/'
+parpath = path+'Parcellations/'
 phenopath = path + 'pheno_data/'
+figdir = path+ 'Figures/'
 
 bad_sub_dict = {}
 
