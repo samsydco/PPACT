ea637125273b7c0d7fc5dcdfc2a495fca30613c0
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-04-15 15:31:59 -0400

    added ISC.py.

---
+++ ISC.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+# Run pairwise ISC
+# Use DF containing: subject, age, ECA, Movie
+# Run ISC in sub-groups
+
+import glob
+import tqdm
+import pandas as pd
+import deepdish as dd
+from itertools import permutations
+from settings import *
+
+
+Phenodf = pd.read_csv(phenopath+'Phenodf.csv')
+Phenodf = Phenodf[Phenodf['FDmean'].notna()].reset_index().drop(['level_0','index'],axis=1)
+
+subs = glob.glob(parpath+'*.h5')
+subpairs = list(permutations(subs,2))
+ROIs = list(dd.io.load(subs[0]).keys())[1:]
+
+for r in tqdm.tqdm(ROIs):
+	
+	
+
+
+
+
+
+
+
+
