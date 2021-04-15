425092101b8e1a6c72e268a2e01c104fde6d42eb
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-04-15 15:31:14 -0400

    Added 4_Parcels.py.

---
+++ 4_Parcels.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Create parcels
+
+import tqdm
+import glob
+import pandas as pd
+import numpy as np
+import deepdish as dd
+import nibabel.freesurfer.io as free
+from settings import *
+
+Phenodf = pd.read_csv(phenopath+'Phenodf.csv')
+Phenodf = Phenodf[Phenodf['FDmean'].notna()].reset_index().drop(['level_0','index'],axis=1)
+
+for sub in tqdm.tqdm(glob.glob(prepath+'*.h5')):
+	roidict = {}
+	for hemi in glob.glob(path+'annot/*'):
+		lab = free.read_annot(hemi)
+		for ri,roi_tmp in tqdm.tqdm(enumerate(lab[2])):
+			roi=roi_tmp.decode("utf-8")
+			roi_short=roi_tmp.decode("utf-8")[11:]
+			vall = np.where(lab[0]==ri)[0]
+			hemi = (hemi.split('/')[-1][0]).upper()
+			roidict[roi_short] = dd.io.load(sub,'/'+hemi,sel=dd.aslice[vall,:])[0]
+	dd.io.save(parpath+sub.split('/')[-1],roidict)
+		
+
+
+
