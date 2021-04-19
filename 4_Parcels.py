09a139217648bbaf2f43b7a2042ea1c67d4508f1
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-04-19 17:15:02 -0400

    Changes to 4_Parcels.py

--- 4_Parcels.py
+++ 4_Parcels.py
@@ -17,12 +17,12 @@ for sub in tqdm.tqdm(glob.glob(prepath+'*.h5')):
 	roidict = {}
 	for hemi in glob.glob(path+'annot/*'):
 		lab = free.read_annot(hemi)
-		for ri,roi_tmp in tqdm.tqdm(enumerate(lab[2])):
+		for ri,roi_tmp in enumerate(lab[2]):
 			roi=roi_tmp.decode("utf-8")
 			roi_short=roi_tmp.decode("utf-8")[11:]
 			vall = np.where(lab[0]==ri)[0]
 			hemi = (hemi.split('/')[-1][0]).upper()
-			roidict[roi_short] = dd.io.load(sub,'/'+hemi,sel=dd.aslice[vall,:])[0]
+			roidict[roi_short] = dd.io.load(sub,'/'+hemi,sel=dd.aslice[vall,:])
 	dd.io.save(parpath+sub.split('/')[-1],roidict)
 		
 
