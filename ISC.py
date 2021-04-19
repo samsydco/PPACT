044603157ba189841d824d828d998c4f9f742493
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-04-19 17:15:11 -0400

    Changes to ISC.py

--- ISC.py
+++ ISC.py
@@ -5,10 +5,13 @@
 # Use DF containing: subject, age, ECA, Movie
 # Run ISC in sub-groups
 
+import os
 import glob
 import tqdm
 import pandas as pd
+import numpy as np
 import deepdish as dd
+from scipy.stats import pearsonr
 from itertools import permutations
 from settings import *
 
@@ -21,9 +24,30 @@ subpairs = list(permutations(subs,2))
 ROIs = list(dd.io.load(subs[0]).keys())[1:]
 
 for roi in tqdm.tqdm(ROIs):
-	for pair in subpairs:
-		v1 = dd.io.load(pair[0],'/'+roi)
-		v2 = dd.io.load(pair[1],'/'+roi)
+	if not any([roi in r for r in glob.glob(ISCdir+'*')]):
+		roidict = {'allvISC':{},'meanISC':{},'patternISC':{}}
+		for pair in subpairs:
+			pairs1 = pair[0].split('/')[-1]
+			pairs2 = pair[1].split('/')[-1]
+			if Phenodf[Phenodf['IDENT_SUBID'] == pairs2[4:9] + '_V2']['MOVIE'].iloc[0] == Phenodf[Phenodf['IDENT_SUBID'] == pairs1[4:9] + '_V2']['MOVIE'].iloc[0]:
+				pairstr = pairs1+' '+pairs2
+				v1 = dd.io.load(pair[0],'/'+roi)
+				v2 = dd.io.load(pair[1],'/'+roi)
+				nvox = nvoxreal = v1.shape[0]
+				goodvox=np.arange(nvox)
+				badvox = []
+				i = 0
+				for v in range(nvox):
+					try:
+						i+=pearsonr(v1[v,:],v2[v,:])[0]
+					except:
+						nvoxreal-=1
+						badvox.append(v)
+				goodvox = np.delete(goodvox, badvox)
+				roidict['allvISC'][pairstr] = i/nvoxreal
+				roidict['meanISC'][pairstr]    = pearsonr(np.nanmean(v1,0),np.nanmean(v2,0))[0]
+				roidict['patternISC'][pairstr] = pearsonr(np.mean(v1[goodvox,:],1),np.mean(v2[goodvox,:],1))[0]
+		dd.io.save(ISCdir+roi+'.h5',roidict)
 		
 	
 
