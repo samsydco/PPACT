aae16d1101ea9214e3ad703265c2a7010b17d068
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-05-14 12:11:05 -0400

    Small modifications to ISC_subplot.

--- ISC_subplot.py
+++ ISC_subplot.py
@@ -22,6 +22,8 @@ figdir_ = figdir+'subcort_ISC/'
 for roi in ROIs:
 	for isc in ['ISCe','ISCb']:
 		fig, ax = plt.subplots()
+		y = 0 if isc=='ISCe' else 1
+		ax.axhline(y=y, color='gray', linestyle='--',linewidth=2)
 		for movie in movies:
 			xticks = []; ISCs = [];
 			for comp in ISCbg[movie][roi]['meanISC'][isc].keys():
@@ -40,9 +42,11 @@ for roi in ROIs:
 	for isc in ['ISCe','ISCb']:
 		fig, ax = plt.subplots(1,3,figsize=(16,5))
 		for event in range(3):
+			y = 0 if isc=='ISCe' else 1
+			ax[event].axhline(y=y, color='gray', linestyle='--',linewidth=3)
 			for movpair in movpairs:
 				xticks = []; ISCs = [];
-				for comp in ISCbg[movie][roi]['meanISC'][isc].keys():
+				for comp in ISCpat[movpair][roi]['patternISC'][event][isc].keys():
 					xticks.append(comp[0]+' vs '+comp[1] if isc == 'ISCb' else comp[0][0]+' vs '+comp[1][0])
 					ISCs.append(ISCpat[movpair][roi]['patternISC'][event][isc][comp])
 					if isc == 'ISCe': xticks=xticks[:4]; ISCs=ISCs[:4]
@@ -50,7 +54,8 @@ for roi in ROIs:
 				if event==0: ax[event].set_ylabel(isc)
 				ax[event].set_title('Event '+str(event))
 		ax[event].legend(bbox_to_anchor=(1, 1.05))
-		fig.savefig(figdir_+roi+'_'+isc+'_pat.png')
+		fig.suptitle(roi, y=1)
+		fig.savefig(figdir_+roi+'_'+isc+'_pat.png', bbox_inches="tight")
 	
 
 
