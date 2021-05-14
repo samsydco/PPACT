6c51dceab66eb86a2a9cff095a497e5de870faf1
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-05-14 14:44:09 -0400

    Added nshuffles to settings.

--- settings.py
+++ settings.py
@@ -15,6 +15,7 @@ ISCdir = path +'ISC/'
 figdir = path+ 'Figures/'
 
 movies = ['Homeward Bound', 'Shirley']
+nshuffle = 100
 
 badsubjs = [['PA231_V2', 'PA092_V2', 'PA164_V2', 'PA077_V2', 'PA155_V2', 'PA074_V2'],['PA042_V2', 'PA017_V2', 'PA085_V2']]
 
