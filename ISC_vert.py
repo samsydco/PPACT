8b024ec8dc3db32957078a15f12f5cca9d9309a1
Samantha Cohen <samantha.s.cohen@gmail.com>
2021-05-27 13:52:44 -0400

    Fixed bug in ISC_vert.

--- ISC_vert.py
+++ ISC_vert.py
@@ -16,7 +16,7 @@ subs = glob.glob(prepath+'*.h5')
 hemis = ['L','R']
 nvox = 40962
 
-ISCdict = {k:{'pairs':[],'ISCs':np.zeros(nvox)} for k in hemis}
+ISCdict = {k:{'pairs':[],'ISCs':np.zeros((nvox,1))} for k in hemis}
 for sub1 in tqdm.tqdm(subs):
 	pair1 = sub1.split('/')[-1]
 	for sub2 in subs:
@@ -28,7 +28,7 @@ for sub1 in tqdm.tqdm(subs):
 				v1 = dd.io.load(sub1,'/'+hemi)
 				v2 = dd.io.load(sub2,'/'+hemi)
 				ISCdict[hemi]['pairs'].append(pairstr1)
-				ISCdict[hemi]['ISCs'] = np.append(ISCdict[hemi]['ISCs'], np.zeros(nvox))
+				ISCdict[hemi]['ISCs'] = np.append(ISCdict[hemi]['ISCs'], np.zeros((nvox,1)))
 				for v in range(nvox):
 					try:
 						ISCdict[hemi]['ISCs'][v,-1] = pearsonr(v1[v,:],v2[v,:])[0]
